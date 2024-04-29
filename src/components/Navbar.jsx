dadd0f23bb979cabfe7aef284e22efb830f14ca5
Askari <askarir64@gmail.com>
2024-04-29 17:53:26 +0530

    initial commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,18 @@
+const Navbar = () => {
+  return (
+    <nav>
+        <div>
+            <h1>Cryp<span>Fi</span></h1>
+            <ul>
+                <li>Home</li>
+                <li>Featured</li>
+                <li>Earn</li>
+                <li>Contact</li>
+            </ul>
+            <button>Connect Wallet</button>
+        </div>
+    </nav>
+  )
+}
+
+export default Navbar
