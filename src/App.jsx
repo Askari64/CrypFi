f5aea64075ce9f27ef3916f4ff62765ee46917a7
Askari <askarir64@gmail.com>
2024-06-28 21:34:35 +0530

    preparing adding more pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,12 @@
-import Featured from "./components/Featured";
-import Featured2 from "./components/Featured2";
-import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
+import Home from "./pages/HomePage"
 
 function App() {
   return (
     <>
       <Navbar />
-      <Hero />
-      <Featured />
-      <Featured2 />
+      <Home />
       <Footer />
     </>
   );
---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,15 @@
+import Hero from "../components/Hero";
+import Featured from "../components/Featured";
+import Featured2 from "../components/Featured2";
+
+function HomePage() {
+  return (
+    <>
+      <Hero />
+      <Featured />
+      <Featured2 />
+    </>
+  );
+}
+
+export default HomePage;
