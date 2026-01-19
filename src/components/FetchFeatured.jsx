634943502b02fb94fc819db454c12273f6591195
Askari <askarir64@gmail.com>
2026-01-19 17:55:46 +0530

    added gaurd for null api response

--- src/components/FetchFeatured.jsx
+++ src/components/FetchFeatured.jsx
@@ -14,6 +14,7 @@ const FetchFeatured = () => {
       .get(url)
       .then((response) => {
         setData(response.data);
+        console.log(response.data);
       })
       .catch((error) => {
         console.log(error);
@@ -22,19 +23,24 @@ const FetchFeatured = () => {
 
   return (
     <div className="grid grid-cols-3 md:grid-cols-5 gap-y-5 justify-center place-items-center">
-      {!data ? Array.from({ length: 20 }, (_, index) => <Skeleton key={index} />) :
-        data.map((fetchedData) => (
-          <Card
-            key={fetchedData.id}
-            imgSrc={fetchedData.image}
-            altText={fetchedData.name}
-            name={fetchedData.name}
-            price={fetchedData.current_price.toLocaleString()}
-            change={fetchedData.price_change_percentage_24h.toFixed(2)}
-          />
-        ))}
+      {!data
+        ? Array.from({ length: 20 }, (_, index) => <Skeleton key={index} />)
+        : data.map((fetchedData) => (
+            <Card
+              key={fetchedData.id}
+              imgSrc={fetchedData.image}
+              altText={fetchedData.name}
+              name={fetchedData.name}
+              price={fetchedData.current_price.toLocaleString() ?? "-"}
+              change={
+                typeof fetchedData.price_change_percentage_24h === "number"
+                  ? fetchedData.price_change_percentage_24h.toFixed(2)
+                  : "0.00"
+              }
+            />
+          ))}
     </div>
   );
-}
+};
 
 export default FetchFeatured;
